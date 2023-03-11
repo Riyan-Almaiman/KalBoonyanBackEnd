74592869fab329c3ace34b1f5b330073bc6ddc98
Riyan <Riyan@DESKTOP-9Q695GJ>
2023-03-11 18:32:42 +0300

    initial server setup

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,22 @@ import cors from 'cors';
 import nodemailer from 'nodemailer'
 import dotenv from 'dotenv'
 import ChatGPTRoutes from './Routes/ChatGPTRoutes'
+import socketio, { Server } from 'socket.io';
+import socketServer from './Controllers/SocketsController';
+
 dotenv.config();
 
 const app:Application = express();
 app.use(cors());
 app.use(express.json());
 
-
 const server = app.listen(3000, () => {
     console.log('Server started on port 3000');
   });
+const io = new Server(server, { cors: { origin: '*' } });
+
+socketServer(server)
+
 
 
 app.use('/chat', ChatGPTRoutes)
